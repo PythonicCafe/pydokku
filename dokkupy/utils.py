570c5b99f4d74fb55bb188536bf9c30b1503f019
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2024-12-26 05:58:40 -0300

    Move plugins code to another module

---
+++ dokkupy/utils.py
@@ -0,0 +1,16 @@
+import datetime
+from functools import lru_cache
+
+
+@lru_cache
+def get_system_tzinfo():
+    return datetime.datetime.now().astimezone().tzinfo
+
+def parse_timestamp(value):
+    return datetime.datetime.fromtimestamp(int(value)).replace(tzinfo=get_system_tzinfo())
+
+def parse_bool(value):
+    value = str(value if value is not None else "").lower()
+    if not value:
+        return None
+    return {"true": True, "t": True, "false": False, "f": False}[value]
