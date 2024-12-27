808f9d3611b54b0da5aa7735b2598730ac33f455
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2024-12-27 16:50:49 -0300

    Move config to the new structure and add more tests

--- dokkupy/plugins/config.py
+++ dokkupy/plugins/config.py
@@ -2,25 +2,31 @@ import base64
 import json
 from typing import List
 
+from ..models import Command
 from .base import DokkuPlugin
 
 
 class ConfigPlugin(DokkuPlugin):
     name = "config"
+    object_class = dict
 
-    def get(self, app_name: str) -> str:
-        # `dokku config <--global|app_name>` does not encode values, so we can't parse correctly if values has newlines
-        # or other special chars. So we first get the keys and then read each key
+    def get(self, app_name: str, merged: bool = False) -> dict:
+        # `dokku config <--global|app_name>` does not encode values, so we can't parse correctly if values have
+        # newlines or other special chars. We use `config:export --format=json` instead.
         system = app_name is None
-        _, stdout, _ = self._execute("export", ["--format=json", "--global" if system else app_name])
+        params = ["--format", "json"]
+        if merged:
+            params.append("--merged")
+        params.append("--global" if system else app_name)
+        stdout = self._evaluate("export", params=params)
         return json.loads(stdout)
 
-    def set_many(self, app_name: str, keys_values: dict, restart: bool = False) -> str:
+    def set_many(self, app_name: str, keys_values: dict, restart: bool = False, execute: bool = True) -> str | Command:
         system = app_name is None
         if system and restart:
             raise ValueError("Cannot restart when setting global config")
         encoded_pairs = {
-            key: base64.b64encode(str(value or "").encode("utf-8")).decode("ascii")
+            key: base64.b64encode(str(value if value is not None else "").encode("utf-8")).decode("ascii")
             for key, value in keys_values.items()
         }
         params = ["--encoded"]
@@ -28,34 +34,34 @@ class ConfigPlugin(DokkuPlugin):
             params.append("--no-restart")
         params.append("--global" if system else app_name)
         params.extend([f"{key}={value}" for key, value in encoded_pairs.items()])
-        _, stdout, _ = self._execute("set", params)
-        return stdout
+        return self._evaluate("set", params=params, execute=execute)
 
-    def set(self, app_name: str, key: str, value: str, restart: bool = False) -> str:
-        return self.set_many(app_name=app_name, keys_values={key: value}, restart=restart)
+    def set(self, app_name: str, key: str, value: str, restart: bool = False, execute: bool = True) -> str | Command:
+        return self.set_many(app_name=app_name, keys_values={key: value}, restart=restart, execute=execute)
 
-    def unset_many(self, app_name: str, keys: List[str], restart: bool = False) -> str:
+    def unset_many(self, app_name: str, keys: List[str], restart: bool = False, execute: bool = True) -> str | Command:
         system = app_name is None
         if system and restart:
-            raise ValueError("Cannot restart when setting global config")
+            raise ValueError("Cannot restart when unsetting global config")
         params = []
         if not restart and not system:
             params.append("--no-restart")
         params.append("--global" if system else app_name)
         params.extend(keys)
-        _, stdout, _ = self._execute("unset", params)
-        return stdout
+        return self._evaluate("unset", params=params, execute=execute)
 
-    def unset(self, app_name: str, key: str, restart: bool = False) -> str:
-        return self.unset_many(app_name=app_name, keys=[key], restart=restart)
+    def unset(self, app_name: str, key: str, restart: bool = False, execute: bool = True) -> str | Command:
+        return self.unset_many(app_name=app_name, keys=[key], restart=restart, execute=execute)
 
-    def clear(self, app_name: str, restart: bool = False) -> str:
+    def clear(self, app_name: str, restart: bool = False, execute: bool = True) -> str | Command:
         system = app_name is None
         if system and restart:
-            raise ValueError("Cannot restart when setting global config")
+            raise ValueError("Cannot restart when clearing global config")
         params = []
         if not restart and not system:
             params.append("--no-restart")
         params.append("--global" if system else app_name)
-        _, stdout, _ = self._execute("clear", params)
-        return stdout
+        return self._evaluate("clear", params=params, execute=execute)
+
+    # TODO: implement `dump`
+    # TODO: implement `ensure_object`
--- tests/test_plugin_config.py
+++ tests/test_plugin_config.py
@@ -1,10 +1,80 @@
+import random
+from string import ascii_letters, digits, punctuation
+
+import pytest
+
 from dokkupy.dokku_cli import Dokku
 
-# TODO: may use dokkupy.inspector to assert the result of each command or mock the command execution and check the
-# to-be-executed command (list of strings)
+
+def random_value(max_chars: int, possible_chars: str = ascii_letters + digits + punctuation + " ") -> str:
+    n_chars = random.randint(3, max_chars)
+    return "".join(random.choice(possible_chars) for _ in range(n_chars))
 
 
-def test_config_set_get():
+def test_set_many_command():
+    app_name = "test-app"
+    pairs = {random_value(8): random_value(64) for _ in range(random.randint(1, 10))}
+    dokku = Dokku()
+    command = dokku.config.set_many(app_name, pairs, restart=False, execute=False)
+    assert command.command[:5] == ["dokku", "config:set", "--encoded", "--no-restart", app_name]
+    assert len(command.command[5:]) == len(pairs)
+    assert command.stdin is None
+    assert command.check is True
+    assert command.sudo is False
+
+    command = dokku.config.set_many(None, pairs, restart=False, execute=False)
+    assert command.command[:4] == ["dokku", "config:set", "--encoded", "--global"]
+    assert len(command.command[4:]) == len(pairs)
+    assert command.stdin is None
+    assert command.check is True
+    assert command.sudo is False
+
+    with pytest.raises(ValueError, match="Cannot restart when setting global config"):
+        dokku.config.set_many(None, pairs, restart=True, execute=False)
+
+
+def test_unset_many_command():
+    app_name = "test-app"
+    keys = [random_value(8) for _ in range(random.randint(1, 10))]
+    dokku = Dokku()
+    command = dokku.config.unset_many(app_name, keys, restart=False, execute=False)
+    assert command.command[:4] == ["dokku", "config:unset", "--no-restart", app_name]
+    assert len(command.command[4:]) == len(keys)
+    assert command.stdin is None
+    assert command.check is True
+    assert command.sudo is False
+
+    command = dokku.config.unset_many(None, keys, restart=False, execute=False)
+    assert command.command[:3] == ["dokku", "config:unset", "--global"]
+    assert len(command.command[3:]) == len(keys)
+    assert command.stdin is None
+    assert command.check is True
+    assert command.sudo is False
+
+    with pytest.raises(ValueError, match="Cannot restart when unsetting global config"):
+        dokku.config.unset_many(None, keys, restart=True, execute=False)
+
+
+def test_clear_command():
+    app_name = "test-app"
+    dokku = Dokku()
+    command = dokku.config.clear(app_name, restart=False, execute=False)
+    assert command.command == ["dokku", "config:clear", "--no-restart", app_name]
+    assert command.stdin is None
+    assert command.check is True
+    assert command.sudo is False
+
+    command = dokku.config.clear(None, restart=False, execute=False)
+    assert command.command == ["dokku", "config:clear", "--global"]
+    assert command.stdin is None
+    assert command.check is True
+    assert command.sudo is False
+
+    with pytest.raises(ValueError, match="Cannot restart when clearing global config"):
+        dokku.config.clear(None, restart=True, execute=False)
+
+
+def test_set_get():
     key1, value1 = "key1", "some value\nwith multiple lines"
     key2, value2 = "key2", 123456  # int value instead of str
     key3, value3 = "key3", True  # bool value instead of str
@@ -21,7 +91,12 @@ def test_config_set_get():
     new_configs = dokku.config.get(app_name=None)
     del expected[key1]
     assert len(expected) == len(new_configs)
-    pairs = {key1: str(value1 or ""), key2: str(value2 or ""), key3: str(value3 or ""), key4: str(value4 or "")}
+    pairs = {
+        key1: str(value1 if value1 is not None else ""),
+        key2: str(value2 if value2 is not None else ""),
+        key3: str(value3 if value3 is not None else ""),
+        key4: str(value4 if value4 is not None else ""),
+    }
     dokku.config.set_many(app_name=None, keys_values=pairs)
     final_configs = dokku.config.get(app_name=None)
     expected = configs_before.copy()
@@ -29,7 +104,30 @@ def test_config_set_get():
     assert final_configs == expected
 
 
-def test_config_clear():
+@requires_dokku
+def test_set_get_merged():
+    app_name = "test-app"
+    keys_local = {"test_a": 1, "test_b": 2, "test_c": 3}
+    keys_global = {"test_a": 0, "test_b": 0, "test_d": 4}
+    dokku = Dokku()
+    initial_global = dokku.config.get(None)
+    dokku.apps.create(app_name)
+    assert dokku.config.get(app_name, merged=False) == {}
+    expected_merged = {key: str(value) for key, value in initial_global.items()}
+    assert dokku.config.get(app_name, merged=True) == expected_merged
+    dokku.config.set_many(None, keys_global)
+    assert dokku.config.get(app_name, merged=False) == {}
+    expected_merged = {key: str(value) for key, value in {**initial_global, **keys_global}.items()}
+    assert dokku.config.get(app_name, merged=True) == expected_merged
+    dokku.config.set_many(app_name, keys_local, restart=False)
+    assert dokku.config.get(app_name, merged=False) == {key: str(value) for key, value in keys_local.items()}
+    expected_merged = {key: str(value) for key, value in {**initial_global, **keys_global, **keys_local}.items()}
+    assert dokku.config.get(app_name, merged=True) == expected_merged
+    dokku.apps.destroy(app_name)
+
+
+@requires_dokku
+def test_clear():
     pairs = {"k1": "v1", "k2": "v2"}
     dokku = Dokku()
     dokku.config.set_many(app_name=None, keys_values=pairs)
@@ -38,6 +136,3 @@ def test_config_clear():
     dokku.config.clear(app_name=None)
     final_configs = dokku.config.get(app_name=None)
     assert len(final_configs) == 0
-
-
-# TODO: implement tests for Dokku.config.* using an app (instead of global), alternating between restart=True|False
