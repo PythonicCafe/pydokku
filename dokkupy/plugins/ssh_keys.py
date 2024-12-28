d7042a3c54c2f8a2438c703a03fd69e9c8e5e4fc
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2024-12-27 21:51:11 -0300

    Move ssh_keys to the new structure and add tests

--- dokkupy/plugins/ssh_keys.py
+++ dokkupy/plugins/ssh_keys.py
@@ -4,7 +4,9 @@ from dataclasses import asdict, dataclass
 from pathlib import Path
 from typing import List
 
+from ..models import Command
 from ..ssh import KEY_TYPES
+from ..utils import clean_stderr
 from .base import DokkuPlugin
 
 REGEXP_SSH_PUBLIC_KEY = re.compile(f"ssh-({'|'.join(KEY_TYPES)}) AAAA[a-zA-Z0-9+/=]+( [^@]+@[^@]+)?")
@@ -22,9 +24,10 @@ class SSHKey:
 
 class SSHKeysPlugin(DokkuPlugin):
     name = "ssh-keys"
+    object_class = SSHKey
 
-    def list(self) -> List[dict]:
-        _, stdout, stderr = self._execute("list", ["--format", "--json"], check=False)
+    def list(self) -> List[SSHKey]:
+        _, stdout, stderr = self._evaluate("list", ["--format", "json"], check=False, full_return=True)
         if "No public keys found" in stderr:
             return []
         result = []
@@ -33,7 +36,7 @@ class SSHKeysPlugin(DokkuPlugin):
             result.append(SSHKey(name=name, fingerprint=fingerprint, options=item))
         return result
 
-    def add(self, name: str, key: str | Path):
+    def add(self, name: str, key: str | Path, execute: bool = True) -> str | Command:
         """Add a SSH key to Dokku
 
         `key` can be the public key itself or the path to the public key file
@@ -45,13 +48,32 @@ class SSHKeysPlugin(DokkuPlugin):
             else:
                 key = Path(key)
         if isinstance(key, Path):
-            with key.open() as fobj:
+            # Key file is plain ASCII (base64-encoded), so we can read as if it were UTF-8 and have it in `str` instead
+            # of `bytes`
+            with key.open(mode="r") as fobj:
                 key_content = fobj.read()
         if key_content is None:
             raise ValueError(f"Unknown key type: {repr(key)}")
-        _, stdout, _ = self._execute("add", [name], stdin=key_content, sudo=True)
+        result = self._evaluate(
+            "add",
+            params=[name],
+            stdin=key_content + "\n",
+            sudo=True,
+            execute=execute,
+            check=False,
+            full_return=True,
+        )
+        if not execute:
+            return result
+        _, stdout, stderr = result
+        if stderr:
+            raise ValueError(f"Cannot add SSH key: {clean_stderr(stderr)}")
         return stdout
 
-    def remove(self, name: str, is_fingerprint=False) -> str:
-        _, stdout, _ = self._execute("remove", ["--fingerprint", name] if is_fingerprint else [name], sudo=True)
-        return stdout
+    def remove(self, name: str, is_fingerprint: bool = False, execute: bool = True) -> str | Command:
+        # WARNING: Dokku won't throw an error if you try to delete an unexisting key
+        params = ["--fingerprint", name] if is_fingerprint else [name]
+        return self._evaluate("remove", params=params, sudo=True, execute=execute)
+
+    # TODO: implement `dump`
+    # TODO: implement `ensure_object`
--- tests/test_plugin_ssh_keys.py
+++ tests/test_plugin_ssh_keys.py
@@ -1,37 +1,70 @@
+import pytest
+
 from dokkupy.dokku_cli import Dokku
 from tests.utils import requires_dokku
 
-# TODO: may use dokkupy.inspector to assert the result of each command or mock the command execution and check the
-# to-be-executed command (list of strings)
+
+def test_add_command(temp_file):
+    key_name = "myuser"
+    key_path = temp_file
+    key_content = "test 123"
+    with temp_file.open(mode="w") as fobj:
+        fobj.write(key_content)
+    dokku = Dokku()
+
+    command = dokku.ssh_keys.add(name=key_name, key=key_path, execute=False)
+    assert command.command == ["dokku", "ssh-keys:add", key_name]
+    assert command.stdin == key_content + "\n"
+    assert command.check is False
+    assert command.sudo is True
+
+
+def test_remove_command():
+    key_name = "myuser"
+    key_fingerprint = "SHA256:I/Du4ECSnSCpI3Q+CCgg+bWnR0sjwlIVwz2IRCEAwbw"
+    dokku = Dokku()
+
+    command = dokku.ssh_keys.remove(key_name, is_fingerprint=False, execute=False)
+    assert command.command == ["dokku", "ssh-keys:remove", key_name]
+    assert command.stdin is None
+    assert command.check is True
+    assert command.sudo is True
+
+    command = dokku.ssh_keys.remove(key_fingerprint, is_fingerprint=True, execute=False)
+    assert command.command == ["dokku", "ssh-keys:remove", "--fingerprint", key_fingerprint]
+    assert command.stdin is None
+    assert command.check is True
+    assert command.sudo is True
 
 
 @requires_dokku
-def test_add_remove():
+def test_add_remove(temp_file):
     dokku = Dokku()
-    keys = dokku.ssh_keys.list()
-    assert len(keys) == 0
+    keys_before = dokku.ssh_keys.list()
 
     name = "debian"
+    another_name = "turicas"
     content = """
         ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC9bIQ9NsZXsOVy/ho6KmRob3MPDgXdmj3XsRzUUjTgjMOPjrGkzKnKQmT+Cq05eGqYqJJChsbWrbazYsEntfYwqE2UGuYJRCs7zlXs10nXb007QkxBaiGkrJz94zayR/8qt6+geGejVl9I7l8EINRK1+SOvv62+8fc1TWQwnsboY0kMN59eS64Lvq35k3gSFn6ZC03ompqZp1OJFqMW+wT7FHGCm9Hoe0si+XU6GWqIKrjg+1GBLUxdtcmfxmUjiimHwAcof3OYl+iTl0zCykYLvamTVwjNLV9guRJ9sq68ljtmxNEZtMs3SgS1y9my/HYM8LQYeePxCuXAFFu3lh493e/mu4YrMdk4rO+3Fqlkr10im+SkEIo3EmKnCWturUrf2i3d37w2QNnX+77T313yH6FYx826ZxfoDknktVZYEmeVQNHG1903bmFNfoDY+R+PI3Pkn0NCs7uhXLFL+pDYJHw12ys32XALYQXyIQbx2H2NHFlugGTGemqYQhCm5U= debian@localhost
     """.strip()
     fingerprint = "SHA256:XiRjUCWNDCrKwSFRSqhR2kP33fEkDsUKbbwhCbnJXas"
-    dokku.ssh_keys.add(name=name, content=content)
+    dokku.ssh_keys.add(name=name, key=content)
 
-    keys = dokku.ssh_keys.list()
-    assert len(keys) == 1
-    assert keys[0]["name"] == name
-    assert keys[0]["fingerprint"] == fingerprint
+    keys_after = dokku.ssh_keys.list()
+    assert len(keys_before) + 1 == len(keys_after)
+    keys_after_by_name = {key.name: key for key in keys_after}
+    assert name in keys_after_by_name
+    assert keys_after_by_name[name].fingerprint == fingerprint
+    assert "SSHCOMMAND_ALLOWED_KEYS" in keys_after_by_name[name].options
 
-    # (venv) debian@localhost:~$ cat .ssh/id_rsa.pub | sudo dokku ssh-keys:add turicas
-    # Duplicate ssh key name
-    #  !     sshcommand returned an error: 1
+    with pytest.raises(ValueError, match="Duplicate ssh key name"):
+        dokku.ssh_keys.add(name=name, key=content)
 
-    # (venv) debian@localhost:~$ echo "oiioioi" | sudo dokku ssh-keys:add turicasssss
-    #  !     Key specified in is not a valid ssh public key
+    with pytest.raises(ValueError, match="Cannot add SSH key: Key specified in is not a valid ssh public key"):
+        with temp_file.open(mode="w") as fobj:
+            fobj.write("invalid key content")
+        dokku.ssh_keys.add(name=another_name, key=temp_file)
 
-
-# TODO: implement tests for deleting existing key
-# TODO: implement tests for trying to delete a non-existing key
-# TODO: implement tests for SSH (key without passphrase)
-# TODO: implement tests for SSH (key with passphrase)
+    dokku.ssh_keys.remove(name=name, is_fingerprint=False)
+    keys_final = dokku.ssh_keys.list()
+    assert len(keys_before) == len(keys_final)
