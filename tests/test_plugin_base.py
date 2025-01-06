01ce756f9043d0183906fc6ed16c90783c4c272b
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2025-01-06 01:07:04 -0300

    Implement end-to-end test (clean-dump-load)

---
+++ tests/test_plugin_base.py
@@ -0,0 +1,49 @@
+import json
+import subprocess
+from copy import deepcopy
+from pathlib import Path
+
+from dokkupy import Dokku
+from tests.utils import requires_dokku
+
+
+def dokku_dump():
+    # TODO: unify with dokkupy/cli.py (without the logging)
+    dokku = Dokku()
+    data = {
+        "dokku": {"version": dokku.version()},
+    }
+    apps = dokku.apps.list()
+    for name, plugin in dokku.plugins.items():
+        data[name] = plugin.dump_all(apps, system=True)
+    return data
+
+
+def dokku_load(data):
+    # TODO: unify with dokkupy/cli.py (without the logging)
+    dokku = Dokku()
+    for key, values in sorted(data.items()):
+        if key == "dokku":
+            continue
+        plugin = getattr(dokku, key)
+        objects = [plugin.object_class(**row) for row in values]
+        for result in plugin.create_objects(objects, execute=True):
+            continue
+
+
+@requires_dokku
+def test_dump_load():
+    current_path = Path(__file__).parent
+    scripts_path = current_path.parent / "scripts"
+    cleanup_script = str((scripts_path / "cleanup.sh").absolute())
+    create_test_env_script = str((scripts_path / "create-test-env.sh").absolute())
+    subprocess.run([cleanup_script], capture_output=True)
+    subprocess.run([create_test_env_script], capture_output=True)
+    data_1 = dokku_dump()
+    subprocess.run([cleanup_script], capture_output=True)
+    dokku_load(deepcopy(data_1))
+    data_2 = dokku_dump()
+    subprocess.run([cleanup_script], capture_output=True)
+    for app in data_1["apps"] + data_2["apps"]:
+        app["created_at"] = None
+    assert data_1 == data_2
