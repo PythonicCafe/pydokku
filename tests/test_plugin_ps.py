4edee05a3036389a50acfb46369235b48359a33c
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2025-01-11 15:16:09 -0300

    Add tests to calculated dataclass attributes

--- tests/test_plugin_ps.py
+++ tests/test_plugin_ps.py
@@ -11,6 +11,20 @@ def test_object_classes():
     assert dokku.ps.object_classes == (ProcessInfo,)
 
 
+def test_process_info_procfile_path_property():
+    app_name = "test-app-1"
+    info_1 = ProcessInfo(
+        app_name=app_name, deployed=False, processes=[], can_scale=True, restart_policy="unless-stopped", restore=True,
+        running=False, global_procfile_path=Path("Procfile"), app_procfile_path=None,
+    )
+    info_2 = ProcessInfo(
+        app_name=app_name, deployed=False, processes=[], can_scale=True, restart_policy="unless-stopped", restore=True,
+        running=False, global_procfile_path=Path("Procfile"), app_procfile_path=Path("Procfile.dokku"),
+    )
+    assert info_1.procfile_path == Path("Procfile")  # Global `procfile_path`, since app's is None
+    assert info_2.procfile_path == Path("Procfile.dokku")  # App's own `procfile_path`
+
+
 def test_start_command():
     app_name = "test-app-1"
     dokku = Dokku()
