8b326ad8dbcea219647227b7a08fe1d6ea785543
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2025-02-05 04:41:51 -0300

    Replace Popen.wait with communicate + add timeout
    It's safer, since it reads the stdout/err buffers and the process won't
    hang waiting for them to be read. Also, kills the process in the end.

--- pydokku/ssh.py
+++ pydokku/ssh.py
@@ -53,7 +53,7 @@ def start_process(command) -> subprocess.Popen:
     )
 
 
-def key_requires_password(filename: Union[Path, str]) -> bool:
+def key_requires_password(filename: Union[Path, str], timeout: float = 5.0) -> bool:
     """Use `ssh-keygen` to check if a SSH key file is password-protected
 
     `ssh-keygen` is used to print the public key related to a private one. If the key is password-protected, the
@@ -61,11 +61,14 @@ def key_requires_password(filename: Union[Path, str]) -> bool:
     """
     command = ["ssh-keygen", "-y", "-f", str(Path(filename).expanduser().absolute())]
     process = start_process(command)
-    process.stdin.close()
-    return process.wait() != 0
+    try:
+        process.communicate(timeout=timeout)
+    finally:
+        process.kill()
+    return process.returncode != 0
 
 
-def key_create(filename: Union[Path, str], key_type: str, password: Union[str, None] = None) -> str:
+def key_create(filename: Union[Path, str], key_type: str, password: Union[str, None] = None, timeout: float = 10.0) -> str:
     """Use `ssh-keygen` to create a new SSH key"""
     if key_type not in KEY_TYPES:
         raise ValueError(f"Invalid SSH key type: {repr(key_type)}")
@@ -80,14 +83,17 @@ def key_create(filename: Union[Path, str], key_type: str, password: Union[str, N
         filename.unlink()
     command = ["ssh-keygen", "-t", key_type, "-f", str(filename), "-N", str(password if password is not None else "")]
     process = start_process(command)
-    result = process.wait()
+    try:
+        stdout, stderr = process.communicate(timeout=timeout)
+    finally:
+        process.kill()
+    result = process.returncode
     if result != 0:
-        stderr = process.stderr.read().strip()
-        raise RuntimeError(f"Error creating SSH key: {stderr}")
-    return process.stdout.read().strip()
+        raise RuntimeError(f"Error creating SSH key: {stderr.strip()}")
+    return stdout.strip()
 
 
-def key_unlock(filename: Union[Path, str], password: str) -> Path:
+def key_unlock(filename: Union[Path, str], password: str, timeout: float = 5.0) -> Path:
     """Copy the SSH key to a temp file and uses `ssh-keygen` to unlock and overwrite the newly created file"""
     filename = Path(filename).expanduser().absolute()
     temp = tempfile.NamedTemporaryFile(delete=False, prefix="pydokku-")
@@ -97,11 +103,14 @@ def key_unlock(filename: Union[Path, str], password: str) -> Path:
         out_fobj.write(in_fobj.read())
     command = ["ssh-keygen", "-p", "-P", password, "-N", "", "-f", str(temp_filename)]
     process = start_process(command)
-    result = process.wait()
+    try:
+        _, stderr = process.communicate(timeout=timeout)
+    finally:
+        process.kill()
+    result = process.returncode
     if result != 0:
         temp_filename.unlink()
-        stderr = process.stderr.read().strip()
-        raise RuntimeError(f"Error unlocking SSH key: {stderr}")
+        raise RuntimeError(f"Error unlocking SSH key: {stderr.strip()}")
     return temp_filename
 
 
@@ -117,20 +126,22 @@ def unlock_key(filename: Union[Path, str], password: str):
             temp_key.unlink()
 
 
-def key_fingerprint(filename_or_content: Union[Path, str]) -> str:
+def key_fingerprint(filename_or_content: Union[Path, str], timeout: float = 5.0) -> str:
     """Extract a fingerprint from a public SSH key"""
     if isinstance(filename_or_content, str) and REGEXP_SSH_PUBLIC_KEY.findall(filename_or_content):
         # `filename_or_content` is the key content, so we use `ssh-keygen`'s stdin
         command = ["ssh-keygen", "-lf", "-"]
-        process = start_process(command)
-        process.stdin.write(filename_or_content)
-        process.stdin.close()
+        stdin = filename_or_content
     else:  # `filename_or_content` is the key path
         filename = Path(filename_or_content).expanduser().absolute()
         command = ["ssh-keygen", "-lf", str(filename)]
-        process = start_process(command)
-    result = process.wait()
+        stdin = None
+    process = start_process(command)
+    try:
+        stdout, stderr = process.communicate(input=stdin, timeout=timeout)
+    finally:
+        process.kill()
+    result = process.returncode
     if result != 0:
-        stderr = process.stderr.read().strip()
-        raise RuntimeError(f"Error reading SSH key fingerprint: {stderr}")
-    return process.stdout.read().strip()
+        raise RuntimeError(f"Error reading SSH key fingerprint: {stderr.strip()}")
+    return stdout.strip()
